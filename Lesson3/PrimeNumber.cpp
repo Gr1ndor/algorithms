7d07a1bc82acb56db2dec765057fc7d595cd9db3
Grigoriy <grifonking98@yandex.ru>
2022-03-24 18:50:18 +0400

    Lesson 3

---
+++ Lesson3/PrimeNumber.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    int x = 0, i = 3;
+    std::cout << "Enter x\n";
+    std::cin >> x;
+    if (x%2 > 0 || x == 2)
+    {
+        while (i*i <= x)
+        {
+            if (x % i == 0)
+            {
+                std::cout << "No prime\n";
+                return 0;
+            }
+            i += 2;
+        }
+        std::cout << "Prime\n";
+        return 0;
+    }
+    else
+    {
+        std::cout << "No prime\n";
+    }
+    return 0;
+}
